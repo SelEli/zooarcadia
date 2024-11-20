49d6c111df1781d7e91f0cfba4966617b7fa75ec
EliSel <seleli@live.fr>
2024-11-21 00:25:34 +0100

    Revert Comment MVC - MongoDB

--- src/Form/CommentsType.php
+++ src/Form/CommentsType.php
@@ -14,27 +14,9 @@ class CommentsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-<<<<<<< HEAD
-            ->add('name', TextType::class, [
-                'label' => 'Nom',
-            ])
-            ->add('comment', TextareaType::class, [
-                'label' => 'Commentaire',
-            ]);
-
-        $comment = $options['data'];
-        if (($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_EMPLOYEE')) && !$comment->isVisible()) {
-            $builder->add('isVisible', CheckboxType::class, [
-                'label' => 'Visible',
-                'required' => false,
-                'mapped' => true,
-            ]);
-        }
-=======
             ->add('name', TextType::class)
             ->add('comment', TextareaType::class);
             // On n'inclut pas le champ 'isVisible' dans le formulaire de création
->>>>>>> parent of aaf67b6 (Ajout du MongoDB pour Comments MVC)
     }
 
     public function configureOptions(OptionsResolver $resolver): void
